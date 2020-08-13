e2e02f67fb5ec310e1db516988d339ba8ecb4d9b
athar1001 <40000893+athar1001@users.noreply.github.com>
2020-08-13 12:00:04 +0200

    Update latest_project.php

--- latest_project.php
+++ latest_project.php
@@ -29,7 +29,7 @@ $insert=mysql_query("INSERT INTO table (title,photo) VALUES ('$title','$file_nam
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
 
